a90b9b8ef8afad277906928be03a0cb8a7ddf619
Sheena Ocon <oconsheen@gmail.com>
2025-03-24 16:52:26 +0800

    Added stats for User on Profile page

--- view/users/includes/topbar.php
+++ view/users/includes/topbar.php
@@ -189,7 +189,7 @@ $fullName = isset($_SESSION["authUser"]["fullName"]) ? $_SESSION["authUser"]["fu
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <a class="dropdown-item d-flex align-items-center" href="../users/users-profile.php">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
               </a>
