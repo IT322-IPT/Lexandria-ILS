b0eac8b5bd7667e161bf8c14b152ad48184eff31
Sheena Ocon <oconsheen@gmail.com>
2025-03-27 06:44:27 +0800

    Added books sa dashboard

--- view/users/index.php
+++ view/users/index.php
@@ -13,7 +13,31 @@ include("../../dB/config.php");
 include("./includes/header.php");
 include("./includes/topbar.php");
 include("./includes/sidebar.php");
+
+$query = "SELECT title, author, isbn FROM books";
+$result = mysqli_query($conn, $query);
 ?>
+
+<div class="container mt-4">
+    <div class="row">
+        <?php
+        while ($row = mysqli_fetch_assoc($result)) {
+            // Encode spaces properly to match filenames
+            $imagePath = "../../assets/img/books/" . rawurlencode($row['title']) . ".jpg";
+            ?>
+            <div class="col-lg-3 mb-4">
+                <div class="card">
+                    <img src="<?= $imagePath ?>" class="card-img-top" alt="<?= htmlspecialchars($row['title']) ?>" 
+                         onerror="this.src='../../assets/img/books/default.jpg'">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= htmlspecialchars($row['title']) ?></h5>
+                        <p class="card-text"><strong>Author:</strong> <?= htmlspecialchars($row['author']) ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
 
 <?php
 include("./includes/footer.php");
